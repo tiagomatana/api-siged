0ce817ca3b26de43d34e5e334ac4bc5103cba6b7
Tiago Matana <tiago@withleaf.io>
2023-05-08 08:42:25 -0300

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 3001;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
 app.get("/api", (req, res) => res.type('json').send({name: 'tiago'}));
 
